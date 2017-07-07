ae5669d221923390f1348606201bdfba9d9a232b
erinfry6 <efry@uchicago.edu>
2017-07-07 16:56:59 -0500

    modified original model file script for variable rates model

--- code/create_model_files.sh
+++ code/create_model_files.sh
@@ -16,19 +16,9 @@ export path=/Users/lynchlab/Desktop/ErinFry/workflowr/AGER ##full absolute path 
 	export pathData=${path}/data/forBAGER
 	export pathScripts=${path}/code
 	export pathResults=${path}/data/BAGERresults/${tissue}
-	export pathTemp=${pathResults}/temporary
-	export pathModelResults=${pathResults}/Models
-	export pathSSSResults=${pathResults}/SSS
 	export pathCommands=${pathScripts}/commands
-	
-	## define pathways of files to be created and utilized around during analysis
-	export scriptversion=$tissue  ## be sure to save files for their tissue as to not confuse
-	export singleexpression=${pathTemp}/singlegene_$scriptversion.txt
-	export model=${pathTemp}/Model_$scriptversion.bin
-	export commandfile=${pathCommands}/step1command_$scriptversion.txt
-	
-###########################################################
-	
+	export pathTemp=${pathResults}/temporary
+
 	## make directories if they do not already exist
 	
 	if [ -e ${pathResults} ]; then
@@ -37,116 +27,138 @@ export path=/Users/lynchlab/Desktop/ErinFry/workflowr/AGER ##full absolute path 
     mkdir ${pathResults}
     fi 
 	
-	if [ -e ${pathTemp} ]; then
-   	echo 'Temporary dir already here'
-    else
-    mkdir ${pathTemp}
-    fi 
 	
 	if [ -e ${pathCommands} ]; then
    	echo 'Command dir already here'
     else
     mkdir ${pathCommands}
-    fi
+    fi	
     
-	## make directory to store the results from the chain
-	if [ -e ${pathMCMCResults} ]; then
-   	echo 'MCMC Results dir already here'
+	if [ -e ${pathTemp} ]; then
+   	echo 'Command dir already here'
     else
-    mkdir ${pathMCMCResults}
-    fi
-    
-	## make directory to store the model from the chain
-	if [ -e ${pathModelResults} ]; then
-   	echo 'MCMC Results dir already here'
-    else
-    mkdir ${pathModelResults} ${pathModelResults}/kappa ${pathModelResults}/delta ${pathModelResults}/none ${pathModelResults}/kd
-    fi
-    
-    ## make directory to store the results from the stepping stone sampler
-	if [ -e ${pathSSSResults} ]; then
-   	echo 'SSS Results dir already here'
-    else
-    mkdir ${pathSSSResults} ${pathSSSResults}/kappa ${pathSSSResults}/delta ${pathSSSResults}/none ${pathSSSResults}/kd
-    fi
+    mkdir ${pathTemp}
+    fi	
 
+###########################################################
+
+	
+	## define the models to be tested
+	models="VarRates Lambda"
+	
+	## make a list of .txt files to be created from the model, these should blank in the *.txt______.txt
+	## if using VarRates with stepping stone sampler these will be the files created
+	filestomake="AncStates Log Schedule Stones VarRates"
+	
+	for f in $filestomake 
+	do
+		mkdir ${pathResults}/$f
+		for m in $models
+		do
+			mkdir ${pathResults}/$f/$m
+		done
+	done
+
+	## make directories for the output tree files for each model
+	mkdir ${pathResults}/Output_trees
+	for m in $models
+		do
+			mkdir ${pathResults}/Output_trees/$m
+		done
+	
+	## make directories for the output MCMC files for each model
+	mkdir ${pathResults}/MCMC
+	for m in $models
+		do
+			mkdir ${pathResults}/MCMC/$m
+		done
+
+
+###########################################################
+	
+	## define pathways of files to be created and utilized during analysis
+	export scriptversion=$tissue  ## be sure to save files for their tissue as to not confuse, or this can be used to run multiple create_model_files at once
+	export singleexpression=${pathResults}/temporary/singlegene_$scriptversion.txt
+	export commandfile=${pathCommands}/step1command_$scriptversion.txt
+	
 ###########################################################
 
 	## Read in files for tissue of interest
 	Expressiondata=${pathData}/${tissue}_exp.txt
 	
 	## number of genes evaluating
-	#NumGenes=$(awk '{print NF}' $Expressiondata | tail -n 1)
-	NumGenes=3
-	
+	NumGenes=$(awk '{print NF}' $Expressiondata | tail -n 1)
+	#NumGenes=10
 	echo Evaluating ${NumGenes} genes in ${tissue}
 
-	## define tree file
+	## define tree input file
 	tree=${pathData}/${tissue}_tree.tree
 
 ###########################################################
 
-	## Run MCMC under each model, saving the summary and log likelihood outputs
+	## Run MCMC under each model, saving all output files
 
 	## The descriptions of each section are explained in the following lines with labels to the right of the commands in the scripts, but generally:
 	## for each model to be tested, make the appropriate command file, then for each of the genes in your expression data file,
-	## create two files summarizing the MCMC run for that trait's evolution and find the likelihood of the model given the data
-	
-	## models to test: name each of the models you are testing
-	## be sure that a directory was created for each of the models (of the same name) in ${pathModelResults}
+	## run BayesTraits to infer evolutionary rate parameters of the gene's expression across the tree, including ancestral states and rates along branches
 	
 	## general commands: you may modify these general commands referring to the properties of the MCMC, see the BayesTraits manual for more details
-	## It is not recommended to remove or modify the general commands that are written here. '4' and '2' indicated a continuous trait random walk model.
-	## using the stepping stone sampler is strongly recommended (stones command)
+	## 12 is **** special****
+	## VarRates tests the variable rates model, a model that breaks the Brownian Motion assumption
+	## Using the stepping stone sampler is strongly recommended (stones command)
 	## Any additional commands you add to each of the models tested should be added here
 	
 	## model-specific commands: these commands are the lines necessary to run each of the models you wish to test
 	
-	## second general commands: these commands are necessary to save model files and run the program, do not modify
+	## run the program, do not modify
 	
-	## create models for each gene: for each gene in 2 through the number of genes in the expression data file, run BayesTraits and save the necessary files
+	## run model for each gene: for each gene in 2 through the number of genes in the expression data file (unless otherwise noted above), run BayesTraits and save output files
 	## each gene will be modeled under each model specified in models to test, and saved accordingly
 
-for c in kd kappa delta none; 											## models to test
-	do
+for m in $models 
+do
 
-echo '4
-2
-Iterations 1010000
-Burnin 10000
-stones 100 10000' > ${commandfile}  									## general commands
+echo '12
+VarRates
+stones 100 10000' > ${commandfile}  								## general commands
 
-if [[ $c == none ]]; then
-echo creating 'none' model files for ${tissue} 						## model-specific commands
-
-elif [[ $c == kappa ]]; then
-echo creating kappa model files for ${tissue}
-echo kappa >> ${commandfile}
-
-elif [[ $c == kd ]]; then
-echo creating kappa and delta model files for ${tissue}
-echo 'kappa
-delta' >> ${commandfile}
+if [[ $m == VarRates ]]; then										## model-specific commands
+echo creating variable rates model files for ${tissue} 
 	
+elif [[ $m == Lambda ]]; then
+echo creating variable rates and lambda model files for ${tissue} 
+echo lambda >> ${commandfile}
+
+
 else 
-echo creating delta model files for ${tissue}
-echo delta >> ${commandfile}
+echo bad model call
 fi
 
-echo SaveModels $model >> ${commandfile} 							## second general commands
-echo run >> ${commandfile}
+
+echo run >> ${commandfile} 										## second general commands
 
 		
 
-	for ((a=2; a<=$NumGenes; a++))										## create models for each gene
+	for ((a=2; a<=$NumGenes; a++))										## run model for each gene
 		do
 		awk -v a="$a" '{print $1,$a}' ${Expressiondata} > ${singleexpression}
 
-		./../BayesTraits/BayesTraitsV3 ${tree} ${singleexpression} <${commandfile} > ${pathTemp}/MCMC.txt || exit 1
-		cp ${singleexpression}.Stones.txt ${pathSSSResults}/${c}/gene$a.txt || exit 1
-		cp ${model} ${pathModelResults}/${c}/gene$a.bin || exit 1
+		./../BayesTraits/BP3.1 ${tree} ${singleexpression} <${commandfile} > ${pathTemp}/MCMC.txt
+		
+for f in $filestomake 
+do
+cp ${singleexpression}.$f.txt ${pathResults}/$f/$m/gene$a.txt
+done
+		
+cp ${pathTemp}/MCMC.txt ${pathResults}/MCMC/$m/gene$a.txt
+cp ${singleexpression}.Output.trees ${pathResults}/Output_trees/$m/gene$a.trees 
+	
+	
+	
+		
+		# || exit 1
 
-		done		
+done		
 
 
 done
